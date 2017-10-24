1b8b082c5bf5a0909a0954330a99c627196526e5
Komron Aripov <aripovkamron@gmail.com>
2017-10-24 18:05:24 +0200

    Adds the student name to the Results.txt file, fix bugs

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import functions
 
 
+# return the value to a local variable
+student_name = functions.username()
+
 # welcome message
 print("""\nHello. This is a 20 question test that will measure your ability to
 accurately add, subtract and divide decimals up to two decimal places. Any
@@ -11,11 +14,10 @@ print("\n")
 for i in range(1,21):
     print("Problem {}".format(i))
     functions.new_problem()
-    print("Current score:", functions.score_counter, "\n")
+    num_score = functions.score_counter
+    print("Current score:", num_score, "\n")
 
-# make variables more readible
-num_score = functions.score_counter
-percent_score = int(functions.score_counter / 20 * 100)
+percent_score = int(functions.score_counter / 20 * 100) # variable introduced locally
 
 # output to user
 print("""Your overall score is {} out of 20. That is a percentage of {}%, which
@@ -23,6 +25,7 @@ is your grade for the test.""".format(num_score, percent_score))
 
 # sends the user's answers, the computer generated problems and the score count to a file
 with open("Results.txt", "w") as result_file:
+    result_file.write("Student name: {}\n\n".format(student_name))
     result_file.writelines(functions.answers_list)
     result_file.write("\nOverall score: {} out of 20.".format(num_score))
     result_file.write("\nPercentage as a score: {}%".format(percent_score))
